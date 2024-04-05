beca28508831ae3b8af3b7205f4bdb31238a137e
Turann_ <84424530+turannul@users.noreply.github.com>
2024-04-05 10:17:30 +0300

    Update LL.m

--- linklock/LL.m
+++ linklock/LL.m
@@ -11,18 +11,19 @@
 - (instancetype)init { return self; }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
     NSString *macModel = [self getMacModel];
     NSString *majorVersionStr = [[NSProcessInfo processInfo] operatingSystemVersion].majorVersion == 0 ? @"" : [NSString stringWithFormat:@"%ld", [[NSProcessInfo processInfo] operatingSystemVersion].majorVersion];
     NSString *minorVersionStr = [[NSProcessInfo processInfo] operatingSystemVersion].minorVersion == 0 ? @"" : [NSString stringWithFormat:@".%ld", [[NSProcessInfo processInfo] operatingSystemVersion].minorVersion];
     NSString *patchVersionStr = [[NSProcessInfo processInfo] operatingSystemVersion].patchVersion == 0 ? @"" : [NSString stringWithFormat:@".%ld", [[NSProcessInfo processInfo] operatingSystemVersion].patchVersion];
 
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{ _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil]; });
+    dispatch_async(dispatch_get_main_queue(), ^{ if (![self hasAccessibilityPermission]) { [self displayAccessibilityAlert]; } });
+    
     NSLog(@"Hello World, LinkLock is running on %@ macOS %@%@%@", macModel, majorVersionStr, minorVersionStr, patchVersionStr);
-
-    if (![self hasAccessibilityPermission]) { [self displayAccessibilityAlert]; } //. Request if not available accessibility permission
 }
 
 // * https://developer.apple.com/documentation/corebluetooth/cbmanagerstate?language=objc
+// * /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreBluetooth.framework/Headers/CBCentralManager.h
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
     switch (central.state) {
       case CBManagerStateUnknown: //. 0
@@ -31,8 +32,8 @@
             break;
       case CBManagerStateResetting: //. 1
             NSLog(@"Bluetooth is resetting... (State: %ld)", (long)central.state);
-            // ! Handle resetting state (not necessary because) 
-            // ! killall -9 bluetoothd took less than a second to recover/reload on my m1 mbp
+            // ! The connection with the system service was momentarily lost, update imminent.
+            // ! killall -9 bluetoothd took less than a second to recover/reload on my m1 mbp < no need to worry about or handling >
             break;
       case CBManagerStateUnsupported: //. 2
             NSLog(@"This device won't support Bluetooth LE (Low Energy). (State: %ld)", (long)central.state);
@@ -61,52 +62,57 @@
     // note: Processing all (4) different status will prevent crash.
     switch (status) {
         case CBManagerAuthorizationNotDetermined: //. 0 
-            NSLog(@"No Perm, requesting?. (Auth Status: %ld)", (long)status);
-            [_centralManager authorize]; // * Requests permission
+            NSLog(@"Permission not requested yet. (Auth Status: %ld)", (long)status);
+            // ! This is not required actually
+            // .[_centralManager requestAuthorizationWithOptions:nil]; // * Request permission
             break;
         case CBManagerAuthorizationDenied: // . 2
-            NSLog(@"No Perm, declined. (Auth Status: %ld)", (long)status);
+            NSLog(@"Permission requested before and it was declined. (Auth Status: %ld)", (long)status);
             [self displayBluetoothAlert]; // * Show dialog
             break;
         case CBManagerAuthorizationAllowedAlways: // . 3
-            NSLog(@"We have bluetooth now! (Auth Status: %ld)", (long)status);
+            NSLog(@"We have permission. (Auth Status: %ld)", (long)status);
             [_centralManager scanForPeripheralsWithServices:nil options:nil]; // * Start scanning
             // * Start scanning for devices - Multiple references calls it peripherals
             break;
         case CBManagerAuthorizationRestricted: // . 4
-            NSLog(@"No Permission, restricted. (Auth Status: %ld)", (long)status);
+            NSLog(@"Bluetooth is restricted. (Auth Status: %ld)", (long)status);
             [self displayBluetoothAlert];
             // * Restricted meaning (According to Apple); In this state, the user can’t change the Bluetooth authorization status, possibly due to active restrictions such as parental controls.
             break;
     }
 }
 
-- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI error:(NSError *)error {
-    //. Peripheral has a valid name and not nil (or "null")
-    if (peripheral.name && ![peripheral.name isEqualToString:@"null"]) { // Ignore peripherals with name "null"
-        NSLog(@"Discovered device: %@, ID: %@, SQ: %@", peripheral.name, peripheral.identifier.UUIDString, RSSI);
-    } else if (error) {
-        NSLog(@"Error while discovering: %@", error);
-    }
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *, id> *)advertisementData RSSI:(NSNumber *)RSSI {
+    BOOL hasValidName = peripheral.name != nil && ![peripheral.name isEqualToString:@"null"]; //. Memory safety included.
+    // * If has valid name and hide is Enabled show devices, otherwise don't show/log
+    BOOL hideUnidentified = YES; //[[NSUserDefaults standardUserDefaults] boolForKey:@"hideUnidentified"]; // * Normally this has to be enabled in settings, default also YES;
+    if (hasValidName && hideUnidentified) { // Both has to be true in order to hide Unknown Devices.
+            NSLog(@"Discovered device: %@, ID: %@, SQ: %@", peripheral.name, peripheral.identifier.UUIDString, RSSI); // ? RSSI = Signal Strength
+        } /* else {
+            NSLog(@"Ignoring unknown devices");
+        } */
 }
+
 
 - (BOOL)hasAccessibilityPermission {
     BOOL accessibilityAccess = AXIsProcessTrusted();
-    NSLog(@"Accessibility permission is %@", accessibilityAccess ? @"available" : @"N/A, no fun :(");
+    NSLog(@"Accessibility permission is %@", accessibilityAccess ? @"available (Trusted: 1)" : @"not available (Trusted: 0)");
     return accessibilityAccess;
 }
+
 //. UI Alerts
 - (void)displayAccessibilityAlert {
     NSAlert *accessibilityAlert = [[NSAlert alloc] init];
     [accessibilityAlert setMessageText:@"Missing Accessibility permission"];
-    [accessibilityAlert setInformativeText:@"Because of security restrictions, Accessibility permission is required to restrict HID (Human Interface Device) events."];
+    [accessibilityAlert setInformativeText:@"Accessibility permission is required for LinkLock to restrict HID (Human Interface Device) devices."];
     [accessibilityAlert addButtonWithTitle:@"Open Settings"];
     [accessibilityAlert addButtonWithTitle:@"Exit"];
     [accessibilityAlert setAlertStyle:NSAlertStyleCritical];
 
     NSModalResponse response = [accessibilityAlert runModal];
     if (response == NSAlertFirstButtonReturn) {
-        NSURL *securityPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility"];
+        NSURL *securityPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility"]; //. Shortcut of: System Preferences -> Security & Privacy -> Accessibility
         [[NSWorkspace sharedWorkspace] openURL:securityPrefsURL];
     } else if (response == NSAlertSecondButtonReturn) {
         exit(0);
@@ -116,14 +122,14 @@
 - (void)displayBluetoothAlert {
     NSAlert *bluetoothAccessAlert = [[NSAlert alloc] init];
     [bluetoothAccessAlert setMessageText:@"Missing Bluetooth permission"];
-    [bluetoothAccessAlert setInformativeText:@"LinkLock needs bluetooth permission to play with it."];
+    [bluetoothAccessAlert setInformativeText:@"Bluetooth permission is required for LinkLock to monitor Bluetooth devices."];
     [bluetoothAccessAlert addButtonWithTitle:@"Open Settings"];
     [bluetoothAccessAlert addButtonWithTitle:@"Exit"];
     [bluetoothAccessAlert setAlertStyle:NSAlertStyleCritical];
 
     NSModalResponse response = [bluetoothAccessAlert runModal];
     if (response == NSAlertFirstButtonReturn) {
-        NSURL *bluetoothPermPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.preference.security?Privacy_Bluetooth"];
+        NSURL *bluetoothPermPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.preference.security?Privacy_Bluetooth"]; //. Shortcut of: System Preferences -> Security & Privacy -> Bluetooth 
         [[NSWorkspace sharedWorkspace] openURL:bluetoothPermPrefsURL];
     } else if (response == NSAlertSecondButtonReturn) {
         exit(0);
@@ -133,13 +139,13 @@
 - (void)displayBluetoothOff {
     NSAlert *bluetoothOffAlert = [[NSAlert alloc] init];
     [bluetoothOffAlert setMessageText:@"Bluetooth is turned off"];
-    [bluetoothOffAlert setInformativeText:@"LinkLock requires bluetooth to be enabled."];
+    [bluetoothOffAlert setInformativeText:@"Bluetooth needs to be turned on for LinkLock to monitor Bluetooth devices."];
     [bluetoothOffAlert addButtonWithTitle:@"Open Settings"];
     [bluetoothOffAlert setAlertStyle:NSAlertStyleWarning];
 
     NSModalResponse response = [bluetoothOffAlert runModal];
     if (response == NSAlertFirstButtonReturn) {
-        NSURL *bluetoothPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.BluetoothSettings"];
+        NSURL *bluetoothPrefsURL = [NSURL URLWithString:@"x-apple.systempreferences:com.apple.BluetoothSettings"]; //. Shortcut of: System Preferences -> Bluetooth
         [[NSWorkspace sharedWorkspace] openURL:bluetoothPrefsURL];
     }
 }
@@ -147,7 +153,7 @@
 - (void)displayBluetoothUnsupported {
     NSAlert *bluetoothUnavailableAlert = [[NSAlert alloc] init];
     [bluetoothUnavailableAlert setMessageText:@"Bluetooth is unavailable"];
-    [bluetoothUnavailableAlert setInformativeText:@"LinkLock needs Bluetooth so you don't have bye."];
+    [bluetoothUnavailableAlert setInformativeText:@"Bluetooth is required for LinkLock to monitor Bluetooth devices."];
     [bluetoothUnavailableAlert addButtonWithTitle:@"Exit"];
     [bluetoothUnavailableAlert setAlertStyle:NSAlertStyleCritical];
 
